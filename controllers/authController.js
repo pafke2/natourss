e89cb8ebb8ec9f1432078be378c9cf621f7f2a76
Baur <baur178@mail.ru>
2026-07-13 21:54:43 +0500

    Added remote server config

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,17 +11,16 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
 
   // REMOVE PASSWORD FROM OUTPUT
   user.password = undefined;
@@ -50,7 +49,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
   // console.log(url);
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -67,7 +66,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
 
   // SEND TOKEN TO THE CLIENT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -223,7 +222,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // UPDATE CHANGEDPASSWORDAT PROPERTY FOR THE USER
   // LOG THE USER IN, SEND JWT
 
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -240,5 +239,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save();
 
   // LOG USER IN, SEND JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
