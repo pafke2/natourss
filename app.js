89de89ad0749f292095d3e19fd76199261995d51
Baur <baur178@mail.ru>
2026-07-13 15:44:16 +0500

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// START EXPRESS APP
 const app = express();
 
 app.set('view engine', 'pug');
